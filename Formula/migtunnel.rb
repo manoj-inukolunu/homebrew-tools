ef4a746c5fda76762ff512f959071bb980611346
manoj-inukolunu <manoj.inukolunu.1989@gmail.com>
2023-06-18 21:12:46 +0530

    Brew formula update for migtunnel version v0.2.9

--- Formula/migtunnel.rb
+++ Formula/migtunnel.rb
@@ -5,20 +5,20 @@
 class Migtunnel < Formula
   desc "Tunneling tool like ngrok and localtunnel"
   homepage "https://github.com/manoj-inukolunu/homebrew-tools"
-  version "0.2.8"
+  version "0.2.9"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.8/migtunnel_Darwin_arm64.tar.gz"
-      sha256 "bb7e086bada7e0a4c6997342f028573d12befd76e5ac7756644ba10efc0c21e9"
+      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.9/migtunnel_Darwin_arm64.tar.gz"
+      sha256 "87407644a8f647aec73f22098429a2bccb655e7719d55a6dd74333f43178bd14"
 
       def install
         bin.install "migtunnel"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.8/migtunnel_Darwin_x86_64.tar.gz"
-      sha256 "f80cdbfb645fb3a5972bc45b06914620149bddb7c4df1ff4197e0f38df9d4117"
+      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.9/migtunnel_Darwin_x86_64.tar.gz"
+      sha256 "f13f4241959c7c309e7ba4c2dc8f9c13f7e1b8045b1c75c6e86e090d5d0d5b8c"
 
       def install
         bin.install "migtunnel"
@@ -28,16 +28,16 @@ class Migtunnel < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.8/migtunnel_Linux_arm64.tar.gz"
-      sha256 "12b21365baef317f070321800f5e16df44e91c714797fe35d98aacc42f0181df"
+      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.9/migtunnel_Linux_arm64.tar.gz"
+      sha256 "6b71351322e45a94c447d3a9ac350e15f28ceb1d48a9502c816afdb920e6b7dc"
 
       def install
         bin.install "migtunnel"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.8/migtunnel_Linux_x86_64.tar.gz"
-      sha256 "3fcc30df83a23374ab841118e0822310737b3bde918cdf390da70bf63070fdfe"
+      url "https://github.com/manoj-inukolunu/migtunnel/releases/download/v0.2.9/migtunnel_Linux_x86_64.tar.gz"
+      sha256 "91dddccf164f7b7e0df45a57ea2f0bc26c19858ec1cee3abf31846352bf93517"
 
       def install
         bin.install "migtunnel"
